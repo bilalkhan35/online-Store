b57f4310d07f309ab2a94c3e1ec8d424ef1b381c
bilalkhan35 <alinabilal999@gmail.com>
2025-09-15 19:00:56 +0500

    fix: correct store.js import path for Vercel build

--- src/main.jsx
+++ src/main.jsx
@@ -4,7 +4,7 @@ import "./index.css";
 import App from "./App.jsx";
 
 import { Provider } from "react-redux";
-import store from "../../redux_online_store/src/store.js";
+import store from "./store.js";
 import { NavbarProvider } from "./NavBarContext.jsx";
 import { WishlistProvider } from "./WishlistContext.jsx";
 
