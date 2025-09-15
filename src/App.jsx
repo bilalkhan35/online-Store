629f57f00faa48fa64a93435265f3fdc22b21c9c
bilalkhan35 <alinabilal999@gmail.com>
2025-09-15 19:04:49 +0500

    fix: correct all import paths in App.jsx for Vercel build

--- src/App.jsx
+++ src/App.jsx
@@ -1,18 +1,15 @@
-import Navbar from "../../redux_online_store/src/components/Navbar";
-import Home from "../../redux_online_store/src/pages/Home";
-import Cart from "../../redux_online_store/src/pages/Cart";
-import Wishlist from "../../redux_online_store/src/pages/WishList";
+import Navbar from "./components/Navbar";
+import Home from "./pages/Home";
+import Cart from "./pages/Cart";
+import Wishlist from "./pages/WishList";
 import { BrowserRouter as Router, Routes, Route } from "react-router-dom";
 
 import { useDispatch, useSelector } from "react-redux";
 import { Toaster } from "react-hot-toast";
-import {
-  fetchProduct,
-  calculateTotals,
-} from "../../redux_online_store/src/features/cartSlice";
+import { fetchProduct, calculateTotals } from "./features/cartSlice";
 import { useEffect } from "react";
-import Product from "../../redux_online_store/src/pages/Product";
-import Modal from "../../redux_online_store/src/components/Modal";
+import Product from "./pages/Product";
+import Modal from "./components/Modal";
 function App() {
   const { cartItems } = useSelector((state) => state.cart);
   const isOpen = useSelector((state) => state.modal?.isOpen);
